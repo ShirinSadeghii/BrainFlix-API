c9b83353031274bbeadbb0a45f080e55aed0d53b
Shirin <shirinsadeghi@live.com>
2023-11-03 16:07:47 -0700

    create dotenv file and hardcode image path in JSON file.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const app = express();
+// const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require("uuid");
 const cors = require('cors');
+require('dotenv').config();
+const { PORT } = process.env
 
-
-const apiKey = "d4ab657f-5aa3-46ad-9d45-85406b23cd11"
-
+// const apiKey = "d4ab657f-5aa3-46ad-9d45-85406b23cd11"
+// app.use("/videos", videoRouter);
 app.use(express.json());
 app.use(cors());
+app.use(express.static(`public`));
 
 app.get("/", (req, res) => {
     res.send("welcome to /");
 });
 
-app.listen(8080, function() {
-    console.log("Hello there");
-});
+// app.listen(8080, function() {
+//     console.log("Hello there");
+// });
+
+app.listen(PORT, () => {
+    console.log("Hello meow");
+})
 
 app.get("/videos", (req, res) => {
     console.log("my videos are working!");
@@ -41,7 +48,7 @@ app.post("/videos", (req, res) => {
         id: uuidv4(),
         title: req.body.title,
         channel: "Red Cow",
-        image: "./public/image0.jpeg",
+        image: "image0.jpeg",
         description: req.body.description,
         views: "0",
         likes: "0",
